74b51d6722a736edbf2ec7766b291f979e82e2e1
Valeriy Petrenko <fourcher00@gmail.com>
2025-10-03 14:14:21 -0400

    Added logic for counting the remaining drawer cash from input

--- sript.js
+++ sript.js
@@ -11,21 +11,22 @@ let cid = [
   ["ONE HUNDRED", 100],
 ];
 
-const currencyUnitValues = [
-  { PENNY: 0.01 },
-  { NICKEL: 0.05 },
-  { DIME: 0.1 },
-  { QUARTER: 0.25 },
-  { ONE: 1 },
-  { FIVE: 5 },
-  { TEN: 10 },
-  { TWENTY: 20 },
-  { "ONE HUNDRED": 100 },
-];
+const currencyUnitValues = {
+  PENNY: 0.01,
+  NICKEL: 0.05,
+  DIME: 0.1,
+  QUARTER: 0.25,
+  ONE: 1,
+  FIVE: 5,
+  TEN: 10,
+  TWENTY: 20,
+  "ONE HUNDRED": 100,
+};
 
-const cash = document.getElementById("cash");
+const itemPrice = document.getElementById("item-price");
+const customerCash = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
-const change = document.getElementById("change");
+const changeDue = document.getElementById("change-due");
 const totalDrawer = document.getElementById("total-drawer");
 const cashInDrawer = document.getElementById("cash-in-drawer");
 
@@ -60,7 +61,60 @@ const renderCashAmount = (arr, htmlEl) => {
     .join(``);
 };
 
+const showChangeDue = (msg) => {
+  changeDue.innerText = `${msg}`;
+};
+
+const inputValidation = (input) => {
+  if (parseInt(input) < price) {
+    alert("Customer does not have enough money to purchase the item");
+    return false;
+  } else if (parseInt(input) == price) {
+    showChangeDue(`No change due - customer paid with exact cash`);
+    return false;
+  }
+  return true;
+};
+
+const calculateChangeNeeded = (cashGiven, valuesObj) => {
+  let neededChange = (parseInt(cashGiven) - price).toFixed(2);
+  let changeObj = {};
+  for (let i = Object.values(valuesObj).length - 1; i >= 0; i--) {
+    while (neededChange >= Object.values(valuesObj)[i]) {
+      neededChange = (neededChange - Object.values(valuesObj)[i]).toFixed(2);
+      changeObj[Object.keys(valuesObj)[i]]
+        ? changeObj[Object.keys(valuesObj)[i]]++
+        : (changeObj[Object.keys(valuesObj)[i]] = 1);
+    }
+    if (changeObj[Object.keys(valuesObj)[i]]) {
+      changeObj[Object.keys(valuesObj)[i]] *= Object.values(valuesObj)[i];
+    }
+  }
+  return Object.entries(changeObj);
+};
+
+const calculateRemainingMoney = (moneyNeededArr, currentMoneyArr) => {
+  return currentMoneyArr.map(([name, value]) => {
+    const match = moneyNeededArr.find(([n]) => n === name);
+    if (match) {
+      const [, amountToSubtract] = match;
+      return [name, value - amountToSubtract];
+    }
+    return [name, value];
+  });
+};
+
+const inputToChange = (input) => {
+  if (!inputValidation(input)) {
+    return;
+  }
+  const neededChange = calculateChangeNeeded(input, currencyUnitValues);
+  const remainingDrawerMoney = calculateRemainingMoney(neededChange, cid);
+  return remainingDrawerMoney;
+};
+
 window.onload = () => {
+  itemPrice.innerText = `Item Price: $${price}`;
   totalDrawer.innerText = `Total: $${calculateTotal(cid)}`;
   renderCashAmount(cid, cashInDrawer);
 };
